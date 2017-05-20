b8c75e277e9b4b199805dc4763c2fffc29732960
Bartosz Balukiewicz <bartekbalukiewicz@gmail.com>
2017-05-20 20:49:07 +0200

    initial project commit

---
+++ config-service/src/main/java/me/balukiewicz/bookshelf/config/BookshelfConfigApplication.java
@@ -0,0 +1,12 @@
+package me.balukiewicz.bookshelf.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookshelfConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookshelfConfigApplication.class, args);
+	}
+}
